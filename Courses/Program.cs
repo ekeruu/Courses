5f476ebdb3c7218a790f01e245ef848df90fa77b
ekeruu <ekeruu@mail.ru>
2024-07-10 03:13:39 +0300

    подключение к бд, добавление, просмтори, изменение и удаление в таблице students

--- Courses/Program.cs
+++ Courses/Program.cs
@@ -1,52 +1,75 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System;
 using System.Linq;
-using System.Reflection.Emit;
-using Microsoft.EntityFrameworkCore;
 
-public class Program
+
+public class Students
 {
-    public static void Main()
-    {
-        using (var context = new Context())
-        {
-            // Добавить новые данные
-            var specialty = new Specialty { SpecialtyName = "Computer Science" };
-            context.Specialties.Add(specialty);
-            context.SaveChanges();
-
-            var department = new Department { DepartmentName = "IT Department" };
-            context.Departments.Add(department);
-            context.SaveChanges();
-
-            var group = new Group
-            {
-                GroupName = "CS101",
-                SpecialtyId = specialty.SpecialtyId,
-                DepartmentId = department.DepartmentId
-            };
-            context.Groups.Add(group);
-            context.SaveChanges();
-
-            // Получить данные
-            var groups = context.Groups
-                .Include(g => g.Specialty)
-                .Include(g => g.Department)
-                .ToList();
-
-            foreach (var g in groups)
-            {
-                Console.WriteLine($"Group: {g.GroupName}, Specialty: {g.Specialty.SpecialtyName}, Department: {g.Department.DepartmentName}");
-            }
-        }
-    }
+    [Key]
+    public int StudentsID { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Phone { get; set; }
+    public DateTime RegistrationDate { get; set; }
 }
 
-
-public class TrainingContext : DbContext
+public class Group
 {
-    public DbSet<Student> Students { get; set; }
+    public int GroupID { get; set; }
+    public string GroupName { get; set; }
+    public int SpecialtyID { get; set; }
+    public int DepartmentID { get; set; }
+}
+
+public class Department
+{
+    public int DepartmentID { get; set; }
+    public string DepartmentName { get; set; }
+}
+
+public class Specialty
+{
+    public int SpecialtyID { get; set; }
+    public string SpecialtyName { get; set; }
+}
+
+public class Course
+{
+    public int CourseID { get; set; }
+    public string CourseName { get; set; }
+    public string Subject { get; set; }
+}
+
+public class Teacher
+{
+    public int TeacherID { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public string PhoneNumber { get; set; }
+    public int ExperienceYears { get; set; }
+}
+
+public class CourseSchedule
+{
+    public int CourseScheduleID { get; set; }
+    public int CourseID { get; set; }
+    public int GroupID { get; set; }
+    public int TeacherID { get; set; }
+    public int Hours { get; set; }
+}
+public class Context : DbContext
+{
+   
+
+    public DbSet<Students> Students { get; set; }
     public DbSet<Group> Groups { get; set; }
     public DbSet<Specialty> Specialties { get; set; }
     public DbSet<Department> Departments { get; set; }
@@ -56,97 +79,57 @@ public class TrainingContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=DESKTOP-JU5GO11\COURSES;Database=CoursesDB;Trusted_Connection=True;");
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-JU5GO11\COURSES;Database=CoursesDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+}
+
+
+
+public class Program
+{
+    public static void Main()
     {
-        base.OnModelCreating(modelBuilder);
-        // Additional configuration here (e.g., relationships, indexes, etc.)
+        using (var context = new Context())
+        {
+            // Пример создания нового студента
+            var newStudent = new Students
+            {
+                Name = "Иван",
+                Surname = "Соколов",
+                Patronymic = "Андреевич",
+                BirthDate = new DateTime(2003, 02, 11),
+                Phone = "89229453402",
+                RegistrationDate = DateTime.Now
+            };
+            context.Students.Add(newStudent);
+            context.SaveChanges();
+
+            // Пример чтения списка студентов
+            var students = context.Students.ToList();
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name} {student.Surname}");
+            }
+
+            /*  // Пример обновления данных студента
+              var studentToUpdate = context.Students.FirstOrDefault(s => s.Name == "Иван");
+              if (studentToUpdate != null)
+              {
+                  studentToUpdate.PhoneNumber = "9876543210";
+                  context.SaveChanges();
+              }*/
+
+            /* // Пример удаления студента
+             var studentToDelete = context.Students.FirstOrDefault(s => s.Name == "))");
+             if (studentToDelete != null)
+             {
+                 context.Students.Remove(studentToDelete);
+                 context.SaveChanges();
+             }*/
+        }
     }
-}
-public class Student
-{
-    public int StudentId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string MiddleName { get; set; }
-    public DateTime BirthDate { get; set; }
-    public string PhoneNumber { get; set; }
-
-    public ICollection<Group> Groups { get; set; }
-}
-
-public class Group
-{
-    public int GroupId { get; set; }
-    public string GroupName { get; set; }
-    public int SpecialtyId { get; set; }
-    public int DepartmentId { get; set; }
-
-    public Specialty Specialty { get; set; }
-    public Department Department { get; set; }
-    public ICollection<Student> Students { get; set; }
-    public ICollection<CourseSchedule> CourseSchedules { get; set; }
-}
-
-public class Specialty
-{
-    public int SpecialtyId { get; set; }
-    public string SpecialtyName { get; set; }
-
-    public ICollection<Group> Groups { get; set; }
-}
-
-public class Department
-{
-    public int DepartmentId { get; set; }
-    public string DepartmentName { get; set; }
-
-    public ICollection<Group> Groups { get; set; }
-}
-
-public class Teacher
-{
-    public int TeacherId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string MiddleName { get; set; }
-    public string PhoneNumber { get; set; }
-    public int ExperienceYears { get; set; }
-
-    public ICollection<CourseSchedule> CourseSchedules { get; set; }
-}
-
-public class Course
-{
-    public int CourseId { get; set; }
-    public string CourseName { get; set; }
-    public int LectureTypeId { get; set; }
-    public string Subject { get; set; }
-    public decimal HourlyRate { get; set; }
-
-    public LectureType LectureType { get; set; }
-    public ICollection<CourseSchedule> CourseSchedules { get; set; }
-}
-
-public class LectureType
-{
-    public int LectureTypeId { get; set; }
-    public string LectureTypeName { get; set; }
-
-    public ICollection<Course> Courses { get; set; }
-}
-
-public class CourseSchedule
-{
-    public int CourseScheduleId { get; set; }
-    public int CourseId { get; set; }
-    public int GroupId { get; set; }
-    public int TeacherId { get; set; }
-    public int Hours { get; set; }
-
-    public Course Course { get; set; }
-    public Group Group { get; set; }
-    public Teacher Teacher { get; set; }
 }
